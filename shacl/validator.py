0800c248cc9171e78d157f56f0a4cc6507b44bf2
mjanez <96422458+mjanez@users.noreply.github.com>
2025-01-30 22:10:01 +0100

    Create validator.py

---
+++ shacl/validator.py
@@ -0,0 +1,28 @@
+from pyshacl import validate
+from rdflib import Graph, Literal, RDF, URIRef
+from rdflib.namespace import NamespaceManager, CSVW, DC, DCAT, DCTERMS, DOAP, FOAF, GEO, ODRL2, ORG, OWL, \
+                           PROF, PROV, RDF, RDFS, SDO, SH, SKOS, SOSA, SSN, TIME, \
+                           VOID, XMLNS, XSD
+                           
+data_graph = Graph()
+data_graph.parse("data.rdf", format='xml')
+
+shacl_graph = Graph()
+shacl_graph.parse("shacl_sample.ttl", format='turtle')
+r = validate(data_graph,
+      shacl_graph=shacl_graph,
+      ont_graph=None,
+      inference='none',
+      abort_on_first=False,
+      allow_infos=False,
+      allow_warnings=False,
+      meta_shacl=False,
+      advanced=False,
+      js=False,
+      debug=False)
+
+validity, results_graph, results_text = r
+
+val = open("validation_result.txt", "w")
+val.write(results_text)
+val.close()
